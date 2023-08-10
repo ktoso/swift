4ca1a7f3d949a0597a580e65cb922ece78dd4c1f
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2023-08-10 18:40:31 +0900

    renames

--- test/Concurrency/Runtime/custom_executors_sdk_with_consuming_param_but_impl_owned.swift
+++ test/Concurrency/Runtime/custom_executors_sdk_with_consuming_param_but_impl_owned.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk-concurrency-consuming-job-param) -emit-sil -parse-as-library %s -verify
+// RUN: %target-swift-frontend(mock-sdk: %clang-importer-sdk-concurrency-consuming-job) -emit-sil -parse-as-library %s -verify
 
 // REQUIRES: concurrency
 // REQUIRES: libdispatch
