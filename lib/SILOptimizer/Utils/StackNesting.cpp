65438d99c1edee4010a47e650afdc06e7bf343b9
Konrad Malawski <ktoso@apple.com>
2026-07-16 11:37:37 +0900

    [Concurrency] Rename CancellationScope to TaskCancellationScope
    Global rename across the Swift-visible API, C++ runtime record, runtime
    symbol names, builtins, and enum members:
    
      Swift:
        CancellationScope                          -> TaskCancellationScope
        __withCancellationScope                    -> __withTaskCancellationScope
        _taskPushCancellationScope                 -> _taskPushTaskCancellationScope
        _taskPopCancellationScope                  -> _taskPopTaskCancellationScope
        _taskCancelCancellationScope               -> _taskCancelTaskCancellationScope
    
      Runtime / C++:
        CancellationScopeRecord                    -> TaskCancellationScopeRecord
        swift_task_pushCancellationScope           -> swift_task_pushTaskCancellationScope
        swift_task_popCancellationScope            -> swift_task_popTaskCancellationScope
        swift_task_cancelCancellationScope         -> swift_task_cancelTaskCancellationScope
        TaskStatusRecordKind::CancellationScope    -> TaskStatusRecordKind::TaskCancellationScope
        ActiveTaskStatus flag HasCancellationScope -> HasTaskCancellationScope
          (and hasCancellationScope / withCancellationScope / withoutCancellationScope)
    
      Builtins:
        Builtin.cancellationScopePush/Pop/Cancel   -> Builtin.taskCancellationScopePush/Pop/Cancel
        BuiltinValueKind::CancellationScope*       -> BuiltinValueKind::TaskCancellationScope*
        StackAllocationKind::BuiltinCancellationScopePush -> BuiltinTaskCancellationScopePush
    
      Files (git-mv, history preserved):
        stdlib/public/Concurrency/CancellationScope.swift  -> TaskCancellationScope.swift
        test/Concurrency/Runtime/cancellation_scope.swift  -> task_cancellation_scope.swift
    
    Also updates the entries in stdlib/public/Concurrency/CMakeLists.txt and
    Runtimes/Core/Concurrency/CMakeLists.txt.

--- lib/SILOptimizer/Utils/StackNesting.cpp
+++ lib/SILOptimizer/Utils/StackNesting.cpp
@@ -203,7 +203,7 @@ static void createDealloc(SILBuilder &B, SILLocation loc, SILInstruction *alloc)
   case StackAllocationKind::BuiltinAddTaskLocalValue:
   case StackAllocationKind::BuiltinTaskAddPriorityEscalationHandler:
   case StackAllocationKind::BuiltinTaskAddCancellationHandler:
-  case StackAllocationKind::BuiltinCancellationScopePush:
+  case StackAllocationKind::BuiltinTaskCancellationScopePush:
     llvm_unreachable("cannot insert this builtin; not safely reorderable");
     return;
   case StackAllocationKind::AllocPackMetadata:
@@ -238,7 +238,7 @@ static bool isUnreorderableAllocation(StackAllocation allocation) {
   case StackAllocationKind::BuiltinAddTaskLocalValue:
   case StackAllocationKind::BuiltinTaskAddPriorityEscalationHandler:
   case StackAllocationKind::BuiltinTaskAddCancellationHandler:
-  case StackAllocationKind::BuiltinCancellationScopePush:
+  case StackAllocationKind::BuiltinTaskCancellationScopePush:
     return true;
   }
   llvm_unreachable("unknown stack allocation");
